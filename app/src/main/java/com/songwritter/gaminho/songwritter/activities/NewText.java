944b16813dd2f7d745098e6953488421322f75bd
ariche <riche.ant@gmail.com>
2017-08-12 15:37:55 +0200

    add SongLyrics to beans and update activities

--- app/src/main/java/com/songwritter/gaminho/songwritter/activities/NewText.java
+++ app/src/main/java/com/songwritter/gaminho/songwritter/activities/NewText.java
@@ -2,8 +2,12 @@ package com.songwritter.gaminho.songwritter.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.songwritter.gaminho.songwritter.R;
+import com.songwritter.gaminho.songwritter.beans.SongLyrics;
 
 public class NewText extends AppCompatActivity {
 
@@ -11,5 +15,13 @@ public class NewText extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_text);
+    }
+
+    public void saveLyrics(View view){
+
+        String title = ((EditText) findViewById(R.id.etTitle)).getText().toString();
+        String content = ((EditText) findViewById(R.id.etContent)).getText().toString();
+        SongLyrics songLyrics = new SongLyrics(title, content, "me");
+        Log.i("SongLyrics", songLyrics.toString());
     }
 }
---
+++ app/src/main/java/com/songwritter/gaminho/songwritter/beans/SongLyrics.java
@@ -0,0 +1,59 @@
+package com.songwritter.gaminho.songwritter.beans;
+
+
+public class SongLyrics {
+
+    private String id;
+    private String title;
+    private String content;
+    private String author;
+
+
+    public SongLyrics(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "SongLyrics{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
